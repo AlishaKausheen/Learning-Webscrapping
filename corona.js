7952546cae1a7af194047b54973b9074e48af3b9
AlishaKausheen <alishakausheen@gmail.com>
2024-01-22 01:29:48 -0800

    getting exact data

--- corona.js
+++ corona.js
@@ -1,10 +1,23 @@
 const request = require("request");
+const cheerio = require("cheerio");
+
+
 request('https://www.worldometers.info/coronavirus/', cb);
 function cb(error, response, html) {
     if (error) {
         console.error('error:', error);
     }
     else {
-        console.log('html', html);
+        handleHtml(html);
     }
+}
+function handleHtml(html) {
+    let selTool = cheerio.load(html);
+    let counterArray = selTool(".maincounter-number");
+    for (let i = 0; i < counterArray.length; i++){
+        let data = selTool(counterArray[i]).text();
+        console.log("data",data);
+    }
+
+    
 }
